2212a361e89d2b52f95bbfe2b8e99feda3483aff
Aleksandrgithub <akksdayz@gmail.com>
2021-01-28 02:22:44 -0800

    fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
-var swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper('.swiper1', {
     pagination: {
         el: '.swiper-pagination',
         dynamicBullets: true,
       },
+  });
+
+var swiper = new Swiper('.swiper2', {
+    slidesPerView: 'auto',
+    spaceBetween: 30,
   });
