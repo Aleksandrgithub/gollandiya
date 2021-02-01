1273d26a1c7d1292ecf4ec4d853b07e3bdb3d1cb
Aleksandrgithub <akksdayz@gmail.com>
2021-02-01 06:27:55 -0800

    coll swiper

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 var swiper = new Swiper('.swiper1', {
     pagination: {
         el: '.swiper-pagination',
+    },
+
+});
+
+var swiper = new Swiper('.swiper2', {
+    slidesPerView: 1.7,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'progressbar',
       },
-  });
+});
