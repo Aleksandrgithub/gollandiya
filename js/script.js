7bcfee7755d39391f93a6a5a52c05c4989a3e3d9
Aleksandrgithub <akksdayz@gmail.com>
2021-02-07 03:37:00 -0800

    add fix scroll js func

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-var swiper = new Swiper('.swiper1', {
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-});
-
-var swiper = new Swiper('.swiper2', {
-    slidesPerView: 1.7,
-    spaceBetween: 30,
-    freeMode: true,
-});
+document.querySelector('#btn-1').addEventListener('change', () => {
+    document.querySelector('main').classList.toggle('fix_scroll');
+  });
---
+++ js/swiper.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.swiper1', {
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+});
+
+var swiper = new Swiper('.swiper2', {
+    slidesPerView: 1.7,
+    spaceBetween: 30,
+    freeMode: true,
+});
