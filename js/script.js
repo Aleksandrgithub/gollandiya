f5b551d896292a520e35e96b001271724b8ee70a
Aleksandrgithub <akksdayz@gmail.com>
2021-02-08 04:31:54 -0800

    fix scroll footer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
 document.querySelector('#btn-1').addEventListener('change', () => {
-    document.querySelector('main').classList.toggle('fix_scroll');
+    document.querySelector('main').classList.toggle('fix_scroll') + document.querySelector('footer').classList.toggle('fix_scroll');
   });
