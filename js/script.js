ec1bc9ed843d747a3df685c249865b5df618e7a9
Aleksandrgithub <drozdov.aleksandr123@gmail.com>
2021-02-16 10:01:09 -0800

    up

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 document.querySelector('#btn-1').addEventListener('change', () => {
     document.querySelector('main').classList.toggle('fix_scroll') + document.querySelector('footer').classList.toggle('fix_scroll');
   });
+
+function backToTop() {
+
+    let button1 = $('.back-to-top');
+    let button2 = $('.back-to-main');
+
+    $(window).on('scroll', () => {
+        if ($(this).scrollTop() >= 100) {
+            button1.fadeIn();
+        } else {
+            button1.fadeOut();
+        }
+        button2.fadeIn();
+    });
+
+    button1.on('click', (e) => {
+        e.preventDefault();
+        $('html').animate({scrollTop: 0}, 1000);
+    })
+}
+
+backToTop();
