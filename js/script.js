bfb14937d8473375a24f2f6cf0bfdb8df01e709b
Aleksandrgithub <drozdov.aleksandr123@gmail.com>
2021-02-17 13:41:18 -0800

    fix nav

--- js/script.js
+++ js/script.js
@@ -2,24 +2,60 @@ document.querySelector('#btn-1').addEventListener('change', () => {
     document.querySelector('main').classList.toggle('fix_scroll') + document.querySelector('footer').classList.toggle('fix_scroll');
   });
 
-function backToTop() {
+// function backToTop() {
 
-    let button1 = $('.back-to-top');
-    let button2 = $('.back-to-main');
+//     let button1 = $('.back-to-top');
+//     let button2 = $('.back-to-main');
 
-    $(window).on('scroll', () => {
-        if ($(this).scrollTop() >= 100) {
-            button1.fadeIn();
-        } else {
-            button1.fadeOut();
-        }
-        button2.fadeIn();
-    });
+//     $(window).on('scroll', () => {
+//         if ($(this).scrollTop() >= 100) {
+//             button1.fadeIn();
+//         } else {
+//             button1.fadeOut();
+//         }
+//         button2.fadeIn();
+//     });
 
-    button1.on('click', (e) => {
-        e.preventDefault();
-        $('html').animate({scrollTop: 0}, 1000);
-    })
-}
+//     button1.on('click', (e) => {
+//         e.preventDefault();
+//         $('html').animate({scrollTop: 0}, 1000);
+//     })
+// }
 
-backToTop();
+// backToTop();
+
+document.querySelector('#shopping_collection').addEventListener('click', () => {
+  document.querySelector('.shopping_collection').classList.remove('is-not-showing') + document.querySelector('.food_collection').classList.add('is-not-showing') +
+  document.querySelector('.entertainment_collection').classList.add('is-not-showing')
+});
+
+document.querySelector('#food_collection').addEventListener('click', () => {
+  document.querySelector('.food_collection').classList.remove('is-not-showing') + document.querySelector('.shopping_collection').classList.add('is-not-showing') +
+  document.querySelector('.entertainment_collection').classList.add('is-not-showing') 
+});
+
+document.querySelector('#entertainment_collection').addEventListener('click', () => {
+  document.querySelector('.entertainment_collection').classList.remove('is-not-showing') + document.querySelector('.shopping_collection').classList.add('is-not-showing') +
+  document.querySelector('.food_collection').classList.add('is-not-showing')
+});
+
+
+///
+
+document.querySelector('#first_floor_button').addEventListener('click', () => {
+  document.querySelector('.first_floor').classList.remove('is-not-showing') + document.querySelector('.ground_floor').classList.remove('is-showing') +
+  document.querySelector('.second_floor').classList.remove('is-showing') + document.querySelector('#first_floor_curr').classList.add('floor_curr') +
+  document.querySelector('#second_floor_curr').classList.remove('floor_curr') + document.querySelector('#ground_floor_curr').classList.remove('floor_curr')
+});
+
+document.querySelector('#second_floor_button').addEventListener('click', () => {
+  document.querySelector('.second_floor').classList.add('is-showing') + document.querySelector('.ground_floor').classList.remove('is-showing') +
+  document.querySelector('.first_floor').classList.add('is-not-showing') + document.querySelector('#first_floor_curr').classList.remove('floor_curr') +
+  document.querySelector('#second_floor_curr').classList.add('floor_curr') + document.querySelector('#ground_floor_curr').classList.remove('floor_curr')
+});
+
+document.querySelector('#ground_floor_button').addEventListener('click', () => {
+  document.querySelector('.ground_floor').classList.add('is-showing') + document.querySelector('.second_floor').classList.remove('is-showing') +
+  document.querySelector('.first_floor').classList.add('is-not-showing') + document.querySelector('#first_floor_curr').classList.remove('floor_curr') +
+  document.querySelector('#second_floor_curr').classList.remove('floor_curr') + document.querySelector('#ground_floor_curr').classList.add('floor_curr')
+});
