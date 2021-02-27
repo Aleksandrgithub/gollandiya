0f350b717d10e0ee1870d5263296ca2db55f188f
Aleksandrgithub <drozdov.aleksandr123@gmail.com>
2021-02-27 10:58:46 -0800

    .

--- js/script.js
+++ js/script.js
@@ -2,6 +2,21 @@ document.querySelector('#btn-1').addEventListener('change', () => {
     document.querySelector('main').classList.toggle('fix_scroll') + document.querySelector('footer').classList.toggle('fix_scroll');
 });
 
+$('#menu_rent').on('click', function () {
+  document.getElementById("btn-1").checked = false;
+  document.querySelector('main').classList.remove('fix_scroll') + document.querySelector('footer').classList.remove('fix_scroll');
+})
+
+$('#menu_advertising').on('click', function () {
+  document.getElementById("btn-1").checked = false;
+  document.querySelector('main').classList.remove('fix_scroll') + document.querySelector('footer').classList.remove('fix_scroll');
+})
+
+$('#menu_contacts').on('click', function () {
+  document.getElementById("btn-1").checked = false;
+  document.querySelector('main').classList.remove('fix_scroll') + document.querySelector('footer').classList.remove('fix_scroll');
+})
+
 $('#menu_shop').on('click', function () {
   document.getElementById("btn-1").checked = false;
   document.querySelector('main').classList.remove('fix_scroll') + document.querySelector('footer').classList.remove('fix_scroll');
@@ -80,22 +95,82 @@ $('#entertainment_collection').on('click', function () {
   }
 })
 
-// ///
 
-// document.querySelector('#first_floor_button').addEventListener('click', () => {
-//   document.querySelector('.first_floor').classList.remove('is-not-showing') + document.querySelector('.ground_floor').classList.remove('is-showing') +
-//   document.querySelector('.second_floor').classList.remove('is-showing') + document.querySelector('#first_floor_curr').classList.add('floor_curr') +
-//   document.querySelector('#second_floor_curr').classList.remove('floor_curr') + document.querySelector('#ground_floor_curr').classList.remove('floor_curr')
-// });
 
-// document.querySelector('#second_floor_button').addEventListener('click', () => {
-//   document.querySelector('.second_floor').classList.add('is-showing') + document.querySelector('.ground_floor').classList.remove('is-showing') +
-//   document.querySelector('.first_floor').classList.add('is-not-showing') + document.querySelector('#first_floor_curr').classList.remove('floor_curr') +
-//   document.querySelector('#second_floor_curr').classList.add('floor_curr') + document.querySelector('#ground_floor_curr').classList.remove('floor_curr')
-// });
+ document.querySelector('#first_home_button').addEventListener('click', () => {
+   document.querySelector('.first_home').classList.remove('is-not-showing') + document.querySelector('.ground_home').classList.remove('is-showing') +
+   document.querySelector('.second_home').classList.remove('is-showing') + document.querySelector('#first_home_curr').classList.add('floor_curr') +
+   document.querySelector('#second_home_curr').classList.remove('floor_curr') + document.querySelector('#ground_home_curr').classList.remove('floor_curr')
+ });
 
-// document.querySelector('#ground_floor_button').addEventListener('click', () => {
-//   document.querySelector('.ground_floor').classList.add('is-showing') + document.querySelector('.second_floor').classList.remove('is-showing') +
-//   document.querySelector('.first_floor').classList.add('is-not-showing') + document.querySelector('#first_floor_curr').classList.remove('floor_curr') +
-//   document.querySelector('#second_floor_curr').classList.remove('floor_curr') + document.querySelector('#ground_floor_curr').classList.add('floor_curr')
-// });
+ document.querySelector('#second_home_button').addEventListener('click', () => {
+   document.querySelector('.second_home').classList.add('is-showing') + document.querySelector('.ground_home').classList.remove('is-showing') +
+   document.querySelector('.first_home').classList.add('is-not-showing') + document.querySelector('#first_home_curr').classList.remove('floor_curr') +
+   document.querySelector('#second_home_curr').classList.add('floor_curr') + document.querySelector('#ground_home_curr').classList.remove('floor_curr')
+});
+
+ document.querySelector('#ground_home_button').addEventListener('click', () => {
+   document.querySelector('.ground_home').classList.add('is-showing') + document.querySelector('.second_home').classList.remove('is-showing') +
+   document.querySelector('.first_home').classList.add('is-not-showing') + document.querySelector('#first_home_curr').classList.remove('floor_curr') +
+   document.querySelector('#second_home_curr').classList.remove('floor_curr') + document.querySelector('#ground_home_curr').classList.add('floor_curr')
+});
+
+
+
+
+
+document.querySelector('#first_shop_button').addEventListener('click', () => {
+  document.querySelector('.first_shop').classList.remove('is-not-showing') + document.querySelector('.ground_shop').classList.remove('is-showing') +
+  document.querySelector('.second_shop').classList.remove('is-showing') + document.querySelector('#first_shop_curr').classList.add('floor_curr') +
+  document.querySelector('#second_shop_curr').classList.remove('floor_curr') + document.querySelector('#ground_shop_curr').classList.remove('floor_curr')
+});
+
+document.querySelector('#second_shop_button').addEventListener('click', () => {
+  document.querySelector('.second_shop').classList.add('is-showing') + document.querySelector('.ground_shop').classList.remove('is-showing') +
+  document.querySelector('.first_shop').classList.add('is-not-showing') + document.querySelector('#first_shop_curr').classList.remove('floor_curr') +
+  document.querySelector('#second_shop_curr').classList.add('floor_curr') + document.querySelector('#ground_shop_curr').classList.remove('floor_curr')
+});
+
+document.querySelector('#ground_shop_button').addEventListener('click', () => {
+  document.querySelector('.ground_shop').classList.add('is-showing') + document.querySelector('.second_shop').classList.remove('is-showing') +
+  document.querySelector('.first_shop').classList.add('is-not-showing') + document.querySelector('#first_shop_curr').classList.remove('floor_curr') +
+  document.querySelector('#second_shop_curr').classList.remove('floor_curr') + document.querySelector('#ground_shop_curr').classList.add('floor_curr')
+});
+
+
+
+document.querySelector('#first_food_button').addEventListener('click', () => {
+  document.querySelector('.first_food').classList.remove('is-not-showing') + document.querySelector('.ground_food').classList.remove('is-showing') +
+  document.querySelector('.second_food').classList.remove('is-showing') + document.querySelector('#first_food_curr').classList.add('floor_curr') +
+  document.querySelector('#second_food_curr').classList.remove('floor_curr') + document.querySelector('#ground_food_curr').classList.remove('floor_curr')
+});
+
+document.querySelector('#second_food_button').addEventListener('click', () => {
+  document.querySelector('.second_food').classList.add('is-showing') + document.querySelector('.ground_food').classList.remove('is-showing') +
+  document.querySelector('.first_food').classList.add('is-not-showing') + document.querySelector('#first_food_curr').classList.remove('floor_curr') +
+  document.querySelector('#second_food_curr').classList.add('floor_curr') + document.querySelector('#ground_food_curr').classList.remove('floor_curr')
+});
+
+document.querySelector('#ground_food_button').addEventListener('click', () => {
+  document.querySelector('.ground_food').classList.add('is-showing') + document.querySelector('.second_food').classList.remove('is-showing') +
+  document.querySelector('.first_food').classList.add('is-not-showing') + document.querySelector('#first_food_curr').classList.remove('floor_curr') +
+  document.querySelector('#second_food_curr').classList.remove('floor_curr') + document.querySelector('#ground_food_curr').classList.add('floor_curr')
+});
+
+document.querySelector('#first_entertainment_button').addEventListener('click', () => {
+  document.querySelector('.first_entertainment').classList.remove('is-not-showing') + document.querySelector('.ground_entertainment').classList.remove('is-showing') +
+  document.querySelector('.second_entertainment').classList.remove('is-showing') + document.querySelector('#first_entertainment_curr').classList.add('floor_curr') +
+  document.querySelector('#second_entertainment_curr').classList.remove('floor_curr') + document.querySelector('#ground_entertainment_curr').classList.remove('floor_curr')
+});
+
+document.querySelector('#second_entertainment_button').addEventListener('click', () => {
+  document.querySelector('.second_entertainment').classList.add('is-showing') + document.querySelector('.ground_entertainment').classList.remove('is-showing') +
+  document.querySelector('.first_entertainment').classList.add('is-not-showing') + document.querySelector('#first_entertainment_curr').classList.remove('floor_curr') +
+  document.querySelector('#second_entertainment_curr').classList.add('floor_curr') + document.querySelector('#ground_entertainment_curr').classList.remove('floor_curr')
+});
+
+document.querySelector('#ground_entertainment_button').addEventListener('click', () => {
+  document.querySelector('.ground_entertainment').classList.add('is-showing') + document.querySelector('.second_entertainment').classList.remove('is-showing') +
+  document.querySelector('.first_entertainment').classList.add('is-not-showing') + document.querySelector('#first_entertainment_curr').classList.remove('floor_curr') +
+  document.querySelector('#second_entertainment_curr').classList.remove('floor_curr') + document.querySelector('#ground_entertainment_curr').classList.add('floor_curr')
+});
